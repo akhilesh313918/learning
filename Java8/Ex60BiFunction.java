51c7f9abf5d1ffe20d37663c2200cc5e511c8e23
akhilesh <akhilesh.yaduwanshi@db.com>
2021-06-16 06:55:17 +0530

    Java8 learning 20210616

---
+++ Java8/Ex60BiFunction.java
@@ -0,0 +1,9 @@
+import java.util.function.BiFunction;
+
+public class Ex60BiFunction{
+public static void main(String [] args){
+BiFunction<Integer,Integer,Integer> f = (a,b) -> a*b;
+System.out.println(f.apply(10,20));
+System.out.println(f.apply(100,200));
+}
+}
