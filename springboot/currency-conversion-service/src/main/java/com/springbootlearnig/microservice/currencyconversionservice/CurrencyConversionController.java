e315087207fd730ae36a9f88dd09ec58ea87332a
akhilesh <akhilesh.yaduwanshi@db.com>
2021-06-25 00:07:45 +0530

    springboot currency-conversion microservice added restTemplate

--- springboot/currency-conversion-service/src/main/java/com/springbootlearnig/microservice/currencyconversionservice/CurrencyConversionController.java
+++ springboot/currency-conversion-service/src/main/java/com/springbootlearnig/microservice/currencyconversionservice/CurrencyConversionController.java
@@ -1,10 +1,13 @@
 package com.springbootlearnig.microservice.currencyconversionservice;
 
 import java.math.BigDecimal;
+import java.util.HashMap;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class CurrencyConversionController {
@@ -14,8 +17,19 @@ public CurrencyConversion calculateCurrencyConversion(
 		@PathVariable String to,
 		@PathVariable BigDecimal quantity
 		) {
-	return new CurrencyConversion(1001L,from, to, quantity,
-			BigDecimal.ONE, BigDecimal.ONE, "");
+	
+	HashMap<String, String> UriVariables = new HashMap<String, String>();
+	UriVariables.put("from", from);
+	UriVariables.put("to", to);
+	
+	ResponseEntity<CurrencyConversion> responseEntity = new RestTemplate().getForEntity
+	("http://localhost:8000/curreny-exchange/from/{from}/to/{to}", 
+			CurrencyConversion.class, UriVariables );
+	
+	CurrencyConversion currencyConversion = responseEntity.getBody();
+	
+	return new CurrencyConversion(currencyConversion.getId(),from, to, quantity,
+			currencyConversion.getConversionMultiple(), quantity.multiply(currencyConversion.getConversionMultiple()), currencyConversion.getEnvironment());
 	
 	}
 }
